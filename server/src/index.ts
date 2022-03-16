87613da0a492e080be2d2918976e9768eacca7ab
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2022-03-16 15:17:34 +0100

    Some minimal working example.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,5 @@
-import express from "express";
-import { Server } from "./server";
+import app from "./app";
 
-const app = express();
 const port = Number(process.env.PORT) || 3000;
 
-const server = new Server(app);
-
-server.start(port);
+app.listen(port);
