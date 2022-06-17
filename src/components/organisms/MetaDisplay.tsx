7e1c18f6ab087a90e665f2de6e8a01ead480e25b
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2022-06-17 12:49:50 +0000

    Added last modified to MetaDisplay.

--- src/components/organisms/MetaDisplay.tsx
+++ src/components/organisms/MetaDisplay.tsx
@@ -11,15 +11,23 @@ const MetaDisplay: React.FC = () => {
   const { data } = useGet<{
     krateSize: number;
     totalRecords: number;
+    updatedAt: Date;
   }>(`${url}/meta/${id}`);
 
   return (
     <Stack component={Paper} style={{ padding: 5 }}>
       <Meta
         title="Database Size"
-        text={`${data ? data.krateSize / 1000 : 0} kB`}
+        text={`${data ? data.krateSize / 1000 : "..."} kB`}
       />
-      <Meta title="Inidvidual Items Stored" text={`${data?.totalRecords}`} />
+      <Meta
+        title="Inidvidual Items Stored"
+        text={`${data?.totalRecords || "..."}`}
+      />
+      <Meta
+        title="Last Updated"
+        text={`${data ? new Date(data.updatedAt).toLocaleString() : "..."}`}
+      />
     </Stack>
   );
 };
